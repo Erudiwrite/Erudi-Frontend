35ff9055c09ac1399fed9a5104e969ae86828f96
Paulmuia <paulmuia7511@gmail.com>
2024-09-15 13:51:23 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import { Provider  } from 'react-redux';
 import { store } from './store/store';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
+
+if (process.env.NODE_ENV === 'production')disableReactDevTools()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
--- src/store/store.js
+++ src/store/store.js
@@ -4,5 +4,9 @@ import userReducer from './userSlice'
 export const store = configureStore({
   reducer: {
     user : userReducer
+
+
   },
+
+  devTools:false
 })
